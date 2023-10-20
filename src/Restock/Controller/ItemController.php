e0644909583a11ccf9b5939b217a5d636d629ad1
Kara T <twombly.kara@gmail.com>
2023-10-19 21:13:21 -0400

    ItemController init

--- src/Restock/Controller/ItemController.php
+++ src/Restock/Controller/ItemController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Restock\Controller;
 
-use Laminas\Diactoros\Response\JsonResponse;
+
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
