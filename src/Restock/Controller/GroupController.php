c21d906449840c4036c8ae4911b2d05dde8f99fe
Kenzy Young <zxc114@mocs.utc.edu>
2023-11-07 10:12:11 -0500

    Group delete permissions check Only the owner can delete the group

--- src/Restock/Controller/GroupController.php
+++ src/Restock/Controller/GroupController.php
@@ -51,6 +51,41 @@ class GroupController
     public function deleteGroup(ServerRequestInterface $request, array $args): ResponseInterface
     {
         $group_id = $args['group_id'] ?? '';
+
+        /** @var \Restock\Entity\User $user */
+        $user = $_SESSION['user'];
+        /** @var \Restock\Entity\GroupMember[] $user_groups */
+        // There is surely a less strange way to do this
+        $user_groups = $user->getMemberDetails();
+
+        $owner = false;
+        $member = false;
+        foreach ($user_groups as $group) {
+            if ($group->getGroup()->getId() == $group_id) {
+                $member = true; // Member in the sense that they exist as part of the group
+                $owner = $group->getRole() == \Restock\Entity\GroupMember::OWNER;
+                break;
+            }
+        }
+
+        if ($member && !$owner) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'You do not have permission to delete this group.'
+            ],
+                403
+            );
+        }
+
+        if (!$member) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'You are not a member of this group, or the group does not exist.'
+            ],
+                400
+            );
+        }
+
         /** @var \Restock\Entity\Group $group */
         if ($group = $this->entityManager->getRepository('\Restock\Entity\Group')->findOneBy(
             ['id' => $group_id]
@@ -67,7 +102,7 @@ class GroupController
         }
 
         return new JsonResponse([
-            'result' => 'success',
+            'result' => 'error',
             'message' => 'Error deleting group.'
         ],
             500
