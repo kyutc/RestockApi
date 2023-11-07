d96e5ae76756cf02354a358f09f2ec87e90a7ee3
Kenzy Young <zxc114@mocs.utc.edu>
2023-11-07 13:56:55 -0500

    Implement update group member

--- src/Restock/Controller/GroupController.php
+++ src/Restock/Controller/GroupController.php
@@ -268,9 +268,108 @@ class GroupController
         );
     }
 
-    public function updateGroupMember(ServerRequestInterface $request): ResponseInterface
+    public function updateGroupMember(ServerRequestInterface $request, array $args): ResponseInterface
     {
-        throw new \Exception("Not implemented.");
+        $group_id = $args['group_id'] ?? '';
+        $user_id = $args['user_id'] ?? '';
+        $role = $request->getQueryParams()['role'] ?? '';
+        /** @var \Restock\Entity\User $user */
+        $user = $_SESSION['user'];
+
+        if (empty($group_id) || empty($user_id) || empty($role) || !is_string($role)) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'Required parameter missing.'
+            ],
+                400
+            );
+        }
+
+        // This is a "fix" to prevent an owner from demoting themselves and thus breaking the group.
+        if ($user->getId() == $user_id) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'You cannot change your own group role.'
+            ],
+                400
+            );
+        }
+
+        // TODO: Should *this* be the path to change ownership, or should that be elsewhere?
+        if ($role == \Restock\Entity\GroupMember::OWNER) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'There can only be one owner of a group.'
+            ],
+                400
+            );
+        }
+
+        /** @var \Restock\Entity\GroupMember[] $user_groups */
+        // There is surely a less strange way to do this
+        $user_groups = $user->getMemberDetails();
+
+        $owner = false;
+        $member = false;
+        foreach ($user_groups as $group) {
+            if ($group->getGroup()->getId() == $group_id) {
+                $member = true; // Member in the sense that they exist as part of the group
+                $owner = $group->getRole() == \Restock\Entity\GroupMember::OWNER;
+                break;
+            }
+        }
+
+        if ($member && !$owner) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'You do not have permission to modify members in this group.'
+            ],
+                403
+            );
+        }
+
+        if (!$member) {
+            return new JsonResponse([
+                'result' => 'error',
+                'message' => 'You are not a member of this group, or the group does not exist.'
+            ],
+                400
+            );
+        }
+
+        /** @var \Restock\Entity\GroupMember $group_member */
+        if ($group_member = $this->entityManager->getRepository('\Restock\Entity\GroupMember')->findOneBy(
+            ['group' => $group_id, 'user' => $user_id]
+        )) {
+            try {
+                $group_member->setRole($role);
+            } catch (\InvalidArgumentException) {
+                return new JsonResponse([
+                    'result' => 'error',
+                    'message' => 'Invalid role for group member.'
+                ],
+                    500
+                );
+            }
+
+            $this->entityManager->persist($group_member);
+            $this->entityManager->flush($group_member);
+
+            return new JsonResponse([
+                'result' => 'success',
+                'message' => 'Group member updated.'
+            ],
+                200
+            );
+        }
+
+        // TODO: Does it matter to return an error saying "user is not a member of this group"? It doesn't do anything regardless.
+        return new JsonResponse([
+            'result' => 'error',
+            'message' => 'Error updating group member.'
+        ],
+            500
+        );
     }
 
     public function deleteGroupMember(ServerRequestInterface $request, array $args): ResponseInterface
