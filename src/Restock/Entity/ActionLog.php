d7d7aa49596dad09815a185027a03690ecc48937
AidanJ522 <lqy522@mocs.utc.edu>
2023-11-10 12:21:10 -0500

    Moved createActionLog and log methods to ActionLog.php Updated log methods

--- src/Restock/Entity/ActionLog.php
+++ src/Restock/Entity/ActionLog.php
@@ -2,8 +2,10 @@
 
 namespace Restock\Entity;
 
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\EntityManager;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'action_log', schema: 'restock')]
@@ -22,13 +24,13 @@ class ActionLog
     private string $log_message;
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
-    private \DateTimeImmutable $timestamp;
+    private DateTimeImmutable $timestamp;
 
     public function __construct(Group $group, string $log_message)
     {
         $this->group = $group;
         $this->log_message = $log_message;
-        $this->timestamp = new \DateTimeImmutable('now');
+        $this->timestamp = new DateTimeImmutable('now');
     }
 
     public function getId(): ?int
@@ -58,14 +60,74 @@ class ActionLog
         return $this;
     }
 
-    public function getTimestamp(): \DateTimeImmutable
+    public function getTimestamp(): DateTimeImmutable
     {
         return $this->timestamp;
     }
 
-    public function setTimestamp(\DateTimeImmutable $timestamp): self
+    public function setTimestamp(DateTimeImmutable $timestamp): self
     {
         $this->timestamp = $timestamp;
         return $this;
+    }
+}
+
+class ActionLogger {
+    private EntityManager $entityManager;
+    private User $user;
+    private Group $group;
+
+    public function __construct(EntityManager $entityManager, User $user, Group $group) {
+        $this->entityManager = $entityManager;
+        $this->user = $user;
+        $this->group = $group;
+    }
+
+    public function logUserAddedToGroup(): ActionLog {
+        return $this->createActionLog('User ' . $this->user->getName() . ' added to group ' . $this->group->getName());
+    }
+
+    public function logUserRemovedFromGroup(): ActionLog {
+        return $this->createActionLog('User ' . $this->user->getName() . ' removed from group ' . $this->group->getName());
+    }
+
+    public function logUserLeftGroup(): ActionLog {
+        return $this->createActionLog('User ' . $this->user->getName() . ' left group ' . $this->group->getName());
+    }
+
+    public function logUserRoleChanged(): ActionLog {
+        return $this->createActionLog('User ' . $this->user->getName() . ' role changed in group ' . $this->group->getName());
+    }
+
+    public function logUsernameChanged(): ActionLog {
+        return $this->createActionLog('User ' . $this->user->getName() . ' name changed');
+    }
+
+    public function logItemAddedToPantry(Item $item): ActionLog {
+        return $this->createActionLog('Item ' . $item->getName() . ' added to pantry in group ' . $this->group->getName());
+    }
+
+    public function logItemRemovedFromPantry(Item $item): ActionLog {
+        return $this->createActionLog('Item ' . $item->getName() . ' removed from pantry in group ' . $this->group->getName());
+    }
+
+    public function logItemAddedToShoppingList(Item $item): ActionLog {
+        return $this->createActionLog('Item ' . $item->getName() . ' added to shopping list in group ' . $this->group->getName());
+    }
+
+    public function logItemRemovedFromShoppingList(Item $item): ActionLog {
+        return $this->createActionLog('Item ' . $item->getName() . ' removed from shopping list in group ' . $this->group->getName());
+    }
+
+    public function logGroupNameChanged(): ActionLog {
+        return $this->createActionLog('Group ' . $this->group->getName() . ' name changed');
+    }
+
+    private function createActionLog(string $logMessage): ActionLog {
+        $actionLog = new ActionLog($this->group, $logMessage);
+        $actionLog->setTimestamp(new DateTimeImmutable('now'));
+        $this->entityManager->persist($actionLog);
+        $this->entityManager->flush();
+        return $actionLog;
     }
 }
